1923a6ab7344586a09355010361c2415962835eb
Yin Da <yd219913@alibaba-inc.com>
2023-02-21 12:11:26 +0800

    Feat: perf
    Signed-off-by: Yin Da <yd219913@alibaba-inc.com>

---
+++ util/perf/pprof.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2023 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package perf
+
+import (
+	"net/http"
+	"net/http/pprof"
+	"time"
+
+	"k8s.io/klog/v2"
+)
+
+func StartProfilingServer(pprofAddr string, errChan chan error) {
+	// Start pprof server if enabled
+	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	pprofServer := http.Server{
+		Addr:              pprofAddr,
+		Handler:           mux,
+		ReadHeaderTimeout: 2 * time.Second,
+	}
+
+	klog.InfoS("Starting debug HTTP server", "addr", pprofServer.Addr)
+
+	if err := pprofServer.ListenAndServe(); err != nil {
+		klog.Error(err, "Failed to start debug HTTP server")
+		if errChan != nil {
+			errChan <- err
+		} else {
+			panic(err)
+		}
+	}
+}
