81ec82fb40f6dade0b15b82131d97e07a6e584cb
nwaiyatharee <nattadej.waiyatharee@agoda.com>
2024-09-24 12:49:14 +0700

    test: waiting for cache
    Signed-off-by: nwaiyatharee <nattadej.waiyatharee@agoda.com>

--- controller/client/suite_test.go
+++ controller/client/suite_test.go
@@ -19,6 +19,7 @@ package client_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
@@ -56,7 +57,7 @@ var _ = Describe("Test clients", func() {
 			Ω(_cache.Start(ctx)).To(Succeed())
 		}()
 
-		// Eventually(func() bool { return _cache.WaitForCacheSync(ctx) }).WithTimeout(time.Second).Should(BeTrue(), "cache should be synced within a second")
+		Eventually(func() bool { return _cache.WaitForCacheSync(ctx) }).WithTimeout(time.Second).Should(BeTrue(), "cache should be synced within a second")
 		velaclient.CachedGVKs = "Deployment.apps.v1"
 		_client, err := velaclient.DefaultNewControllerClient(cfg, client.Options{
 			Cache: &client.CacheOptions{
